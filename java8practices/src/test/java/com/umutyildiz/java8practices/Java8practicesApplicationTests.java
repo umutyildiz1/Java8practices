ca2a8db893a3195aac9ef1a62ff4d0e78b2bdf5c
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2022-02-11 17:39:00 +0300

    project template created

---
+++ java8practices/src/test/java/com/umutyildiz/java8practices/Java8practicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutyildiz.java8practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8practicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
