06940147f48eafb48644dbbc2721f72f571316ce
umutyildiz1 <umutyildiz840@gmail.com>
2024-04-18 21:54:41 +0300

    refactor

--- java8practices/src/main/java/Java8/functionalinterfacelambda/functionalinterfaceandlambda.java
+++
@@ -1,7 +0,0 @@
-package Java8.functionalinterfacelambda;
-
-public class functionalinterfaceandlambda {
-    //https://www.baeldung.com/java-8-lambda-expressions-tips
-    //https://www.baeldung.com/java-8-functional-interfaces
-
-}
