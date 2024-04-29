8f421bc78f051d65db5a4625177ed58f9bc03bc4
umutyildiz1 <umutyildiz840@gmail.com>
2024-04-29 21:57:53 +0300

    add singleton

---
+++ java8practices/src/main/java/designpatterns/cretionalpatterns/singleton/Main.java
@@ -0,0 +1,19 @@
+package DesignPatterns.cretionalpatterns.singleton;
+
+import static DesignPatterns.cretionalpatterns.singleton.Singleton.getInstance;
+public class Main {
+    public static void main(String[] args) {
+
+        //Singleton, ensures that only one instance created and provides only one access point
+
+        //Singleton is an antipattern
+
+        //violates the Single Responsibility Principle : solve 2 problem
+
+        //single thread non thread safe example
+        //there are various of singleton like non thread safe multithread and thread safe
+        System.out.println(getInstance("Singleton 1").getValue());
+        System.out.println(getInstance("Singleton 2").getValue());
+        System.out.println(getInstance("Singleton 3").getValue());
+    }
+}
---
+++ java8practices/src/main/java/designpatterns/cretionalpatterns/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package DesignPatterns.cretionalpatterns.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+    private String value;
+
+    private Singleton(String value) {
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value){
+        if (instance == null)
+            instance = new Singleton(value);
+
+        return instance;
+    }
+
+    public String getValue(){
+        return value;
+    }
+}
